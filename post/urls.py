71e467f89831b515a9f281ff0cef1123bd6372c0
sasada <tmkxq-sasadayuto@yahoo.co.jp>
2020-06-12 20:12:36 +0900

    made profile page

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,7 @@ app_name = 'post'
 
 urlpatterns = [
     path('privacy/', views.PrivacyPolicyView.as_view(), name='privacy'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
     path('detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('<str:tag>/', views.IndexView.as_view(), name='index_tag'),
